a1ea09ebe1090c22b8c206c8f185ad34c1244580
wonee1 <gimchaewon417@gmail.com>
2025-09-06 20:47:40 +0900

    스프링 객체 지향 원리 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,40 +9,72 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
+@Configuration
 public class AppConfig {
 
-//    public MemberService memberService() {
-//        // MemoryMemberRepository 객체 생성 → MemberServiceImpl에 주입
-//        return new MemberServiceImpl(new MemoryMemberRepository());
-//    }
-//
-//    public OrderService orderService() {
-//        // MemoryMemberRepository와 FixDiscountPolicy를 생성해서 OrderServiceImpl에 주입
-//        return new OrderServiceImpl(
-//                new MemoryMemberRepository(),
-//                new FixDiscountPolicy());
-//    }
-
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-       // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
-
 }
+
+
+
+
+//public class AppConfig {
+//
+////    public MemberService memberService() {
+////        // MemoryMemberRepository 객체 생성 → MemberServiceImpl에 주입
+////        return new MemberServiceImpl(new MemoryMemberRepository());
+////    }
+////
+////    public OrderService orderService() {
+////        // MemoryMemberRepository와 FixDiscountPolicy를 생성해서 OrderServiceImpl에 주입
+////        return new OrderServiceImpl(
+////                new MemoryMemberRepository(),
+////                new FixDiscountPolicy());
+////    }
+//
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(memberRepository());
+//    }
+//
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(
+//                memberRepository(),
+//                discountPolicy());
+//    }
+//
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//
+//    public DiscountPolicy discountPolicy() {
+//       // return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
+//    }
+//
+//
+//} 스프링 적용 전
