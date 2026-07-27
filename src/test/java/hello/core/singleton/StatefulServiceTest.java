eb3bd41aa27856008145e39bdbd067b31f36cfd7
wonee1 <gimchaewon417@gmail.com>
2026-07-27 17:49:44 +0900

    feat : 싱글톤 컨테이너 테스트

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,38 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class StatefulServiceTest {
+
+
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(StatefulService.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA: A 사용자 10000원 주문
+        statefulService1.order("userA",10000);
+
+        //ThreadB : B 사용자 20000원 주문
+        statefulService2.order("userB",20000);
+
+        //Thread A : 사용자 A 주문 금액 조회
+        int price=statefulService1.getPrice();
+        System.out.println("price =  " + price);
+
+    }
+
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+
+
+}
